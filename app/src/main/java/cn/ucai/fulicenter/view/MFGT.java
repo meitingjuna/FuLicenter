2a8b2efdcce7896fd2e896ceb6a314899e166ab3
meitingjun <1271888057@qq.com>
2017-01-10 15:58:01 +0800

    重新提交

---
+++ app/src/main/java/cn/ucai/fulicenter/view/MFGT.java
@@ -0,0 +1,20 @@
+package cn.ucai.fulicenter.view;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.MainActivity;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.controller.activity.SplashActivity;
+
+/**
+ * Created by MTJ on 2017/1/10.
+ */
+
+public class MFGT {
+    public static  void startActivity(Activity context,Class<?> clz){
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_bottom_out);
+        context.startActivity(new Intent(context,clz));
+
+    }
+}
