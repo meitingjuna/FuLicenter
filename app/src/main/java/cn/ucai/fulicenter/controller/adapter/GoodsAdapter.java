feac9eaf6daa41b04b56d61bbb9ec091d9129443
meitingjun <1271888057@qq.com>
2017-01-11 17:27:36 +0800

    首页面的上拉跟下拉

--- app/src/main/java/cn/ucai/fulicenter/controller/adapter/GoodsAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/adapter/GoodsAdapter.java
@@ -2,7 +2,6 @@ package cn.ucai.fulicenter.controller.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -21,44 +20,107 @@ import cn.ucai.fulicenter.model.ustils.ImageLoader;
  */
 
 public class GoodsAdapter extends RecyclerView.Adapter {
+    private static final int TYPE_GOODS = 0;
+    private static final int TYPE_FOOTER = 1;
     Context mContext;
     ArrayList<NewGoodsBean> mList;
+    boolean isMore;
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    String footer;
+    boolean isDragging;
+
+
+    public boolean isDragging() {
+        return isDragging;
+    }
+
+    public void setDragging(boolean dragging) {
+        isDragging = dragging;
+        notifyDataSetChanged();
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+        notifyDataSetChanged();
+    }
+
+
 
     public GoodsAdapter(Context context, ArrayList<NewGoodsBean> list) {
         mContext = context;
         mList = new ArrayList<>();
         mList.addAll(list);
-
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       RecyclerView.ViewHolder holder=new GoodsViewHolder(View.inflate(mContext,R.layout.item_goods, null));
-
-        return holder;
+        if (viewType == TYPE_GOODS) {
+            RecyclerView.ViewHolder holder =
+                    new GoodsViewHolder(View.inflate(mContext, R.layout.item_goods, null));
+            return holder;
+        } else {
+            RecyclerView.ViewHolder holder =
+                    new FooterViewHolder(View.inflate(mContext, R.layout.item_footer, null));
+            return holder;
+        }
     }
+    //RecyclerView.ViewHolder holder = new GoodsViewHolder(View.inflate(mContext, R.layout.item_goods, null));
+
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        GoodsViewHolder vh = (GoodsViewHolder) holder;
-        ImageLoader.downloadImg(mContext,vh.ivGoodsThumb,mList.get(position).getGoodsThumb());
-        vh.tvGoodsName.setText(mList.get(position).getGoodsName());
-        vh.tvGoodsPrice.setText(mList.get(position).getCurrencyPrice());
+        if (getItemViewType(position) == TYPE_FOOTER) {
+            FooterViewHolder vh = (FooterViewHolder) holder;
+            vh.tvFooter.setText(getFooter());
+            return;
+        }
 
-
+        GoodsViewHolder holder1 = (GoodsViewHolder) holder;
+        ImageLoader.downloadImg(mContext, holder1.ivGoodsThumb, mList.get(position).getGoodsThumb());
+        holder1.tvGoodsName.setText(mList.get(position).getGoodsName());
+        holder1.tvGoodsPrice.setText(mList.get(position).getCurrencyPrice());
     }
 
     @Override
     public int getItemCount() {
-        return mList.size();
+        return mList.size() + 1;
     }
-    public void initData (ArrayList<NewGoodsBean>list){
-        if (mList!=null){
-            mList.addAll(list);
-            notifyDataSetChanged();
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return TYPE_FOOTER;
         }
+        return TYPE_GOODS;
     }
-    static  class GoodsViewHolder extends RecyclerView.ViewHolder{
+
+    public void initData(ArrayList<NewGoodsBean> list) {
+        if (mList != null) {
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public void addData(ArrayList<NewGoodsBean> list) {
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    static class GoodsViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.ivGoodsThumb)
         ImageView ivGoodsThumb;
         @BindView(R.id.tvGoodsName)
@@ -68,8 +130,18 @@ public class GoodsAdapter extends RecyclerView.Adapter {
 
         GoodsViewHolder(View view) {
             super(view);
-            ButterKnife.bind(this,view);
+            ButterKnife.bind(this, view);
 
+        }
+    }
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tvFooter)
+        TextView tvFooter;
+
+        public FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
         }
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/NewGoodsFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/NewGoodsFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,11 +24,15 @@ import cn.ucai.fulicenter.model.net.IModelNewGoods;
 import cn.ucai.fulicenter.model.net.ModelNewGoods;
 import cn.ucai.fulicenter.model.net.OnCompleteListener;
 import cn.ucai.fulicenter.model.ustils.ConvertUtils;
+import cn.ucai.fulicenter.view.SpaceItemDecoration;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewGoodsFragment extends Fragment {
+    static final int ACTION_DOWNLOAD = 0;//下载首页
+    static final int ACTION_PULL_DOWN = 1;//下拉刷新
+    static final int ACTION_PULL_UP = 2;//上拉加载
 
     @BindView(R.id.tvRefresh)
     TextView tvRefresh;
@@ -46,35 +51,95 @@ public class NewGoodsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_new_goods, container, false);
         ButterKnife.bind(this, layout);
-
         initView();
         mModel = new ModelNewGoods();
         initData();
+        setListener();
         return layout;
     }
 
+    private void setListener() {
+        setPullDownListener();
+        setPullUpListener();
+    }
+
+    //上拉
+    private void setPullUpListener() {
+        mRw.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                mAdapter.setDragging(newState == RecyclerView.SCROLL_STATE_DRAGGING);
+                int lastPosition = gm.findLastVisibleItemPosition();
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && mAdapter.isMore() && lastPosition == mAdapter.getItemCount() - 1) ;
+                pageId++;
+                downloadContactLiset(ACTION_PULL_UP, pageId);
+            }
+        });
+
+    }
+
+    //下拉
+    private void setPullDownListener() {
+        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                srl.setRefreshing(true);
+                tvRefresh.setVisibility(View.VISIBLE);
+                pageId = 1;
+                downloadContactLiset(ACTION_PULL_DOWN, pageId);
+            }
+        });
+    }
+
     private void initData() {
+        pageId = 1;
+        downloadContactLiset(ACTION_DOWNLOAD, pageId);
+    }
+
+    private void downloadContactLiset(final int action, int PageId) {
         mModel.downData(getContext(), I.CAT_ID, pageId, new OnCompleteListener<NewGoodsBean[]>() {
             @Override
             public void onSuccess(NewGoodsBean[] result) {
+                mAdapter.setMore(result != null && result.length > 0);
+                if (!mAdapter.isMore()) {
+                    if (action == ACTION_PULL_UP) {
+                        mAdapter.setFooter("没有更多数据了/(ㄒoㄒ)");
+                    }
+                    return;
+                }
+                mAdapter.setFooter("加载更多数据O(∩_∩)");
                 ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
-                mList.addAll(list);
-                mAdapter.initData(list);
+                switch (action) {
+                    case ACTION_DOWNLOAD:
+                        mAdapter.initData(list);
+                        break;
+                    case ACTION_PULL_DOWN:
+                        srl.setRefreshing(false);
+                        tvRefresh.setVisibility(View.GONE);
+                        mAdapter.initData(list);
+                        break;
+                    case ACTION_PULL_UP:
+                        mAdapter.addData(list);
+                        break;
+                }
             }
 
             @Override
             public void onError(String error) {
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
 
             }
         });
     }
+
 
     private void initView() {
         srl.setColorSchemeColors(
@@ -87,7 +152,9 @@ public class NewGoodsFragment extends Fragment {
         mRw.setHasFixedSize(true);
         mAdapter = new GoodsAdapter(getContext(), mList);
         mRw.setAdapter(mAdapter);
+        mRw.addItemDecoration(new SpaceItemDecoration(20));
 
     }
+
 
 }
