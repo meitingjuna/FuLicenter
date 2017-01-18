cbaacd4fba006401c1faf338a824e20d333d0909
meitingjun <1271888057@qq.com>
2017-01-18 18:55:32 +0800

    上传头像,无法上传有待解决

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/SettingsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/SettingsActivity.java
@@ -1,12 +1,14 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,9 +16,16 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
 import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
 import cn.ucai.fulicenter.model.net.SharePrefrenceUtils;
+import cn.ucai.fulicenter.model.ustils.CommonUtils;
 import cn.ucai.fulicenter.model.ustils.ImageLoader;
+import cn.ucai.fulicenter.model.ustils.OnSetAvatarListener;
+import cn.ucai.fulicenter.model.ustils.ResultUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -30,6 +39,8 @@ public class SettingsActivity extends AppCompatActivity {
     TextView tvUserProfileNick;
     @BindView(R.id.iv_fanhui_1)
     ImageView ivFanhui1;
+    OnSetAvatarListener mOnSetAvatarListener;
+    IModelUser model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,19 +80,72 @@ public class SettingsActivity extends AppCompatActivity {
     public void onClick1() {
         MFGT.finish(this);
     }
-
+    //更新昵称
     @OnClick(R.id.layout_user_profile_nickname)
-    public void onClick() {
+    public void onClickNick() {
         MFGT.gotoUpDataNick(this);
+    }
 
+//
+    @OnClick(R.id.layout_user_profile_avatar)
+    public void onClick() {
+        CommonUtils.showShortToast(R.string.username_connot_be_modify);
+    }
+
+    @OnClick(R.id.layout_user_profile_avatar)
+    public void onClickAvatar() {
+        mOnSetAvatarListener = new OnSetAvatarListener(this,
+                R.id.layout_user_profile_avatar,
+                FuLiCenterApplication.getUser().getMuserName(),
+                I.AVATAR_TYPE_USER_PATH);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
         if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_NICK) {
             tvUserProfileNick.setText(FuLiCenterApplication.getUser().getMuserNick());
+        } else if (requestCode == OnSetAvatarListener.REQUEST_CROP_PHOTO) {
+            uploadAvatar();
+        } else {
+            mOnSetAvatarListener.setAvatar(requestCode, data, ivUserProfileAvatar);
         }
+    }
+
+    private void uploadAvatar() {
+        model = new ModelUser();
+        User user = FuLiCenterApplication.getUser();
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.update_user_avatar));
+        dialog.show();
+        File file = null;
+        file = new File(String.valueOf(OnSetAvatarListener.getAvatarFile(this,
+                I.AVATAR_TYPE_USER_PATH + "/" + user.getMuserName() + user.getMavatarSuffix())));
+        model.updatAvatar(this, user.getMuserName(), file, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                int msg = R.string.update_user_avatar_fail;
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    if (result != null) {
+                        if (result.isRetMsg()) {
+                            msg = R.string.update_user_avatar_success;
+                        }
+                    }
+                }
+                CommonUtils.showLongToast(msg);
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showLongToast(R.string.update_user_avatar_fail);
+                dialog.dismiss();
+            }
+        });
     }
 }
 
