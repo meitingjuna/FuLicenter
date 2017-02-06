bbd76b95a7af2b99451b605de3c4b8dcaa214c68
meitingjun <1271888057@qq.com>
2017-02-06 10:37:16 +0800

    购物车Adapter的编写

--- app/src/main/java/cn/ucai/fulicenter/controller/adapter/CartAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/adapter/CartAdapter.java
@@ -1,7 +1,7 @@
 package cn.ucai.fulicenter.controller.adapter;
 
 import android.content.Context;
-import android.provider.ContactsContract;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,78 +9,64 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
-
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.model.bean.CartBean;
 import cn.ucai.fulicenter.model.bean.GoodsDetailsBean;
-import cn.ucai.fulicenter.model.bean.NewGoodsBean;
+import cn.ucai.fulicenter.model.bean.MessageBean;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
 import cn.ucai.fulicenter.model.ustils.ImageLoader;
 
+
 /**
- * Created by LCH on 2017/1/11.
+ * Created by clawpo on 2017/1/20.
  */
 
 public class CartAdapter extends RecyclerView.Adapter {
     Context mContext;
     ArrayList<CartBean> mList;
-    ArrayList<NewGoodsBean> mN;
-    String footer;
+    IModelUser model;
+    User user;
 
-
-    public String getFooter() {
-        return footer;
-    }
-
-    public void setFooter(String footer) {
-        this.footer = footer;
-        notifyDataSetChanged();
-    }
-
-
-    public CartAdapter(Context context, ArrayList<CartBean> list) {
+    public CartAdapter(Context context, ArrayList<CartBean> mlist) {
         mContext = context;
-        mList = new ArrayList<>();
-        mList.addAll(list);
+        this.mList = mlist;
+        model = new ModelUser();
+        user = FuLiCenterApplication.getUser();
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        RecyclerView.ViewHolder holder =
-                new CartViewHolder(View.inflate(mContext, R.layout.item_cart, null));
-        return holder;
+    public CartViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CartViewHolder vh = new CartViewHolder(View.inflate(mContext, R.layout.item_cart, null));
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder ParentHolder, final int position) {
-        CartViewHolder vh = (CartViewHolder) ParentHolder;
-        CartBean cartBean = mList.get(position);
-        GoodsDetailsBean goods = cartBean.getGoods();
-        if (goods != null) {
-            ImageLoader.downloadImg(mContext, vh.mivGoodsThumb, goods.getGoodsThumb());
-            vh.mtvGoodsName.setText(goods.getGoodsName());
-            vh.mtvGoodsPrice.setText(goods.getCurrencyPrice());
-        }
-        vh.mtvCartCount.setText("(" + cartBean.getCount() + ")");
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        CartViewHolder vh = (CartViewHolder) holder;
+        vh.bind(position);
     }
-
 
     @Override
     public int getItemCount() {
-        return mList.size();
+        return mList!=null?mList.size():0;
     }
 
-
     public void initData(ArrayList<CartBean> list) {
-        if (mList != null) {
+        if (mList!=null){
             mList.clear();
         }
-        mList.addAll(list);
-        notifyDataSetChanged();
+        addData(list);
     }
 
     public void addData(ArrayList<CartBean> list) {
@@ -88,25 +74,93 @@ public class CartAdapter extends RecyclerView.Adapter {
         notifyDataSetChanged();
     }
 
-    static class CartViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.chkSelect)
-        CheckBox mchkSelect;
-        @BindView(R.id.ivGoodsThumb)
-        ImageView mivGoodsThumb;
-        @BindView(R.id.tvGoodsName)
-        TextView mtvGoodsName;
-        @BindView(R.id.ivAddCart)
-        ImageView mivAddCart;
-        @BindView(R.id.tvCartCount)
-        TextView mtvCartCount;
-        @BindView(R.id.ivReduceCart)
-        ImageView mivReduceCart;
-        @BindView(R.id.tvGoodsPrice)
-        TextView mtvGoodsPrice;
+    class CartViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_cart_thumb)
+        ImageView mIvCartThumb;
+        @BindView(R.id.tv_cart_good_name)
+        TextView mTvCartGoodName;
+        @BindView(R.id.tv_cart_count)
+        TextView mTvCartCount;
+        @BindView(R.id.tv_cart_price)
+        TextView mTvCartPrice;
+        @BindView(R.id.cb_cart_selected)
+        CheckBox mSelected;
+        int listPosition;
 
         CartViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+        }
+
+        public void bind(int position) {
+            listPosition = position;
+            GoodsDetailsBean detailsBean = mList.get(position).getGoods();
+            if (detailsBean!=null) {
+                ImageLoader.downloadImg(mContext, mIvCartThumb, detailsBean.getGoodsThumb());
+                mTvCartGoodName.setText(detailsBean.getGoodsName());
+                mTvCartPrice.setText(detailsBean.getCurrencyPrice());
+            }
+            mTvCartCount.setText("("+mList.get(position).getCount()+")");
+            mSelected.setChecked(mList.get(listPosition).isChecked());
+        }
+
+        @OnCheckedChanged(R.id.cb_cart_selected)
+        public void checkListener(boolean checked){
+            mList.get(listPosition).setChecked(checked);
+            mContext.sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART));
+        }
+
+        @OnClick(R.id.iv_cart_add)
+        public void addCart(){
+            model.updateCart(mContext, I.ACTION_CART_ADD, user.getMuserName(),
+                    mList.get(listPosition).getGoodsId(), 1, mList.get(listPosition).getId(),
+                    new OnCompleteListener<MessageBean>() {
+                        @Override
+                        public void onSuccess(MessageBean result) {
+                            if (result!=null && result.isSuccess()){
+                                mList.get(listPosition).setCount(mList.get(listPosition).getCount()+1);
+                                mContext.sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART));
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });
+        }
+
+        @OnClick(R.id.iv_cart_del)
+        public void delCart(){
+            final int count = mList.get(listPosition).getCount();
+            int action = I.ACTION_CART_UPDATA;
+            if (count>1){
+                //update
+                action = I.ACTION_CART_UPDATA;
+            }else{
+                //del
+                action = I.ACTION_CART_DEL;
+            }
+            model.updateCart(mContext, action, user.getMuserName(),
+                    mList.get(listPosition).getGoodsId(), count-1, mList.get(listPosition).getId(),
+                    new OnCompleteListener<MessageBean>() {
+                        @Override
+                        public void onSuccess(MessageBean result) {
+                            if (result!=null && result.isSuccess()){
+                                if (count<=1){
+                                    mList.remove(listPosition);
+                                }else {
+                                    mList.get(listPosition).setCount(count - 1);
+                                }
+                                mContext.sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART));
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });
         }
     }
 }
